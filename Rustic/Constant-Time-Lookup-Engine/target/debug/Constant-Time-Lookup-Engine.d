1268ea07b4c7741959c2d1308bf09b15389af7d4
echenim <myron.echenim@gmail.com>
2026-04-21 23:22:11 -0700

    Go and Rust implemtation

--- Rustic/Constant-Time-Lookup-Engine/target/debug/Constant-Time-Lookup-Engine.d
+++
@@ -1,1 +0,0 @@
-/Users/itachi/Dev/Drills/Rustic/Constant-Time-Lookup-Engine/target/debug/Constant-Time-Lookup-Engine: /Users/itachi/Dev/Drills/Rustic/Constant-Time-Lookup-Engine/src/main.rs
--- Rustic/Constant-Time-Lookup-Engine/target/debug/deps/Constant_Time_Lookup_Engine-5ae027db15e38d76.d
+++
@@ -1,5 +0,0 @@
-/Users/itachi/Dev/Drills/Rustic/Constant-Time-Lookup-Engine/target/debug/deps/Constant_Time_Lookup_Engine-5ae027db15e38d76.d: src/main.rs
-
-/Users/itachi/Dev/Drills/Rustic/Constant-Time-Lookup-Engine/target/debug/deps/Constant_Time_Lookup_Engine-5ae027db15e38d76: src/main.rs
-
-src/main.rs:
